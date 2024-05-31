454f53e8324b700469e27ebd29b78a637b25acef
Sayad Uddin Tahsin <89304780+Sayad-Uddin-Tahsin@users.noreply.github.com>
2024-05-31 11:26:33 +0000

    Updated Animation and modified About page
    * Added animation on Landing Page's picture
    * Added animation on black region and header
    * Added 'Projects' button under Experiences in About page
    * Configured 'Projects' button on Landing Page

--- script.js
+++ script.js
@@ -15,12 +15,42 @@ const typewriterEffect = (text, element) => {
 };
 
 window.addEventListener('load', () => {
-    if (typedTitle && typedDescription && buttons) {
-        // Specific logic for index.html
-        typewriterEffect('Sayad Uddin Tahsin', typedTitle);
-        typewriterEffect('Passionate Software Developer and Tech Enthusiast', typedDescription);
+    if (typedTitle) {
+        const title = typedTitle.getAttribute('data-typed');
+        typewriterEffect(title, typedTitle);
+    }
+    if (typedDescription) {
+        const description = typedDescription.getAttribute('data-typed');
+        typewriterEffect(description, typedDescription);
+    }
+    if (buttons) {
         setTimeout(() => {
             buttons.classList.add('fade-visible');
         }, 1000);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var profilePic = document.querySelector('.profile-pic');
+    if (profilePic) {
+        profilePic.addEventListener('load', function() {
+            profilePic.classList.add('loaded');
+        });
+    };
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var blackRegion = document.querySelector('.black-region');
+    var fadeInSection = document.querySelector('.fade-in-page-title');
+    var header = document.querySelector('header');
+
+    if (blackRegion && fadeInSection) {
+        blackRegion.style.opacity = '1';
+        blackRegion.style.height = '25vh';
+        fadeInSection.classList.add('show');
+    }
+    
+    if (header) {
+        header.classList.add('show');
     }
 });
